d307106e5d2fd0e774e0795571f6b76b9f674dd6
yvan99 <ishimweyvan90@gmail.com>
2025-11-04 15:39:53 +0200

    initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
